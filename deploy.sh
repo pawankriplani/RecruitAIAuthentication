43cbf59676a015450ef93fbd5701c5e0c12e4958
mithun <mithunwalawalkar100@gmail.com>
2025-07-14 14:47:54 +0530

    added cors URLs

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Define variables
+PROJECT_ID="telusrecruitai"
+IMAGE_NAME="recruitai-authentication"
+REGION="us-central1"  # Change this to your preferred region
+SERVICE_NAME="recruitai-authentication-service"
+
+# Build the Docker image
+docker build -t $IMAGE_NAME .
+
+# Tag the image for Google Container Registry
+docker tag $IMAGE_NAME gcr.io/$PROJECT_ID/$IMAGE_NAME
+
+# Push the image to Google Container Registry
+docker push gcr.io/$PROJECT_ID/$IMAGE_NAME
+
+# Deploy to Cloud Run
+gcloud run deploy $SERVICE_NAME \
+  --image gcr.io/$PROJECT_ID/$IMAGE_NAME \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated
